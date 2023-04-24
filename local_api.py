b5dcd651f5a36e6f9b86c1b1a563f5d68c4ff12f
felipe.odoni <felipe.odoni@gmail.com>
2023-04-24 21:49:45 +0100

    local llm

---
+++ local_api.py
@@ -0,0 +1,20 @@
+from langchain.llms.base import LLM
+from typing import Optional, List, Mapping, Any
+import requests
+import json
+
+class LocalApi(LLM):
+        
+    @property
+    def _llm_type(self) -> str:
+        return "custom"
+    
+    def _call(self, prompt: str, stop: Optional[List[str]] = None) -> str:
+        print("Prompt: " + prompt)
+        url = "http://127.0.0.1"
+        payload={'message': prompt}
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+        }
+        response = requests.request("POST", url, headers=headers, data=payload)
+        return response.text
--- qa_local.py
+++ qa_local.py
@@ -1,9 +1,9 @@
 """Ask a question to the notion database."""
 import faiss
-from langchain import HuggingFaceHub
 from langchain.chains import VectorDBQAWithSourcesChain
 import pickle
 import argparse
+from .local_api import LocalApi
 
 parser = argparse.ArgumentParser(description='Ask a question to the notion DB.')
 parser.add_argument('question', type=str, help='The question to ask the notion DB')
@@ -17,9 +17,11 @@ with open("faiss_store_local.pkl", "rb") as f:
 
 store.index = index
 
-llm=HuggingFaceHub(repo_id="google/flan-t5-xl", model_kwargs={"temperature":0.7, "max_length":512})
+# llm=HuggingFaceHub(repo_id="google/flan-t5-xl", model_kwargs={"temperature":0.7, "max_length":512})
 
-chain = VectorDBQAWithSourcesChain.from_llm(llm=llm, vectorstore=store)
+
+
+chain = VectorDBQAWithSourcesChain.from_llm(llm=LocalApi(), vectorstore=store)
 result = chain({"question": args.question})
 print(f"Answer: {result['answer']}")
 print(f"Sources: {result['sources']}")
