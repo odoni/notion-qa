26170f38d82da171ad149d92b07c4db30f0bf284
felipe.odoni <felipe.odoni@gmail.com>
2023-04-25 16:31:39 +0100

    fix

--- qa_local.py
+++ qa_local.py
@@ -22,7 +22,7 @@ llm=HuggingFaceHub(repo_id="google/flan-t5-xl", model_kwargs={"temperature":0, "
 # llm=LocalApi()
 
 
-chain = RetrievalQAWithSourcesChain.from_llm(llm=llm, vectorstore=store)
+chain = RetrievalQAWithSourcesChain.from_llm(llm=llm)
 result = chain({"question": args.question})
 print(f"Answer: {result['answer']}")
 print(f"Sources: {result['sources']}")
